bfd5c8492414b4c68dfdaf593fc6107d12583fb3
MarkNahabedian <naha@mit.edu>
2021-12-15 10:02:33 -0500

    Default missing constructors.

--- src/note_BNFNode_location.jl
+++ src/note_BNFNode_location.jl
@@ -4,90 +4,92 @@ using MacroTools
 using MacroTools: postwalk
 
 
+find_name(s::Symbol) = s
+
+function find_name(exp::Expr)
+    @assert length(exp.args) >= 1
+    find_name(exp.args[1])
+end
+
 function rewrite_struct(def::Expr)::Expr
     structpattern = postwalk(rmlines, Meta.parse(
         "struct nameexp_ fields__ end "))
     constructorpattern = postwalk(rmlines, Meta.parse(
         "function fname_(fargs__) fbody__ end"))
     bindings = MacroTools.match(structpattern, def)
-    fields2 = map(bindings[:fields]) do e
+    fields = []
+    constructors = []
+    for e in bindings[:fields]
         e = longdef(e)
         if isexpr(e, :function)
-            bindings2 = MacroTools.match(constructorpattern, e)
-            [
-                # default source argument
-                postwalk(e) do e
-                    if iscall(e, :new)
-                        Expr(:call, :new, nothing, e.args[2:end]...)
-                    else
-                        e
-                    end
-                end,
-                # pass through source argument
-                :(function $(bindings2[:fname])(source::LineNumberNode,
-                                                $(bindings2[:fargs]...))
-                      $(map(bindings2[:fbody]) do e
-                            postwalk(e) do e
-                                if iscall(e, :new)
-                                    Expr(:call, :new, :source, e.args[2:end]...)
-                                else
-                                    e
-                                end
-                            end
-                        end...)
-                  end)
-            ]
+            push!(constructors, e)
         else
-            [e]
+            push!(fields, e)
         end
     end
-    fields2 = cat(fields2...; dims=1)
+    if length(constructors) == 0
+        # Add default constructor
+        name = find_name(bindings[:nameexp])
+        args = map(find_name, fields)
+        push!(constructors,
+              :(function $name($(args...))
+                    new($(args...))
+                end))
+    end
+    constructors2 = []
+    for e in constructors
+        # default source argument
+        push!(constructors2,
+              postwalk(e) do e
+                  if iscall(e, :new)
+                      Expr(:call, :new, nothing, e.args[2:end]...)
+                  else
+                      e
+                  end
+              end)
+        # pass through source argument
+        bindings2 = MacroTools.match(constructorpattern, e)
+        push!(constructors2,
+              :(function $(bindings2[:fname])(source::Union{Nothing, LineNumberNode},
+                                              $(bindings2[:fargs]...))
+                    $(map(bindings2[:fbody]) do e
+                          postwalk(e) do e
+                              if iscall(e, :new)
+                                  Expr(:call, :new, :source, e.args[2:end]...)
+                              else
+                                  e
+                              end
+                          end
+                      end...)
+                end)        )
+    end
     :(struct $(bindings[:nameexp])
           source::Union{Nothing, LineNumberNode}
-          $(fields2...)
+          $(fields...)
+
+          $(constructors2...)
       end)
 end
 
-#=
+
+export @bnfnode
+
 macro bnfnode(exp)
     @capture(exp,
              struct nameexp_
                  fields__
              end)
-    find_name(s::Symbol) = s)
-
-    function find_name(exp::Expr)
-        @assert length(exp.args) >= 1
-        find_name(exp.args[1])
-    end
-    new_fields = []
-    for field in fields
-        @capture(field,
-        function $name(args__)
-            body__
-        end |
-            $name(args__) = body__)
-        if body != nothing
-            # Need to find the call to new and add __source__ at the beginning
-            push!(new_fields, quote
-                      function $name(__source__, args...)
-                      end
-                  end)
-        else
-            push!(new_fields, field)
-        end
-    end
-    
     name = find_name(nameexp)
-    quote
-        struct $nameexp
-            source::LineNumberNode
-            $new_fields...
-        end
+    mname = Symbol("@$name")
 
-        macro $name(args...)
-            :($name(__source__, $args...))
+    quote
+        $(rewrite_struct(exp))
+
+        export $name, $mname
+
+        macro $(esc(name))(args...)
+            Expr(:call, $(esc(name)), $__source__, args...)
         end
     end
 end
-=#
+
