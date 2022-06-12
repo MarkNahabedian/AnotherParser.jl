9c1011f9b5286fa9f446d81969b151b8d35296a5
MarkNahabedian <naha@mit.edu>
2022-06-12 17:03:43 -0400

    show_grammar

--- src/AnotherParser.jl
+++ src/AnotherParser.jl
@@ -3,6 +3,7 @@ module AnotherParser
 include("note_BNFNode_location.jl")
 include("BNFtypes.jl")
 include("check_references.jl")
+include("pprint.jl")
 
 include("../examples/BNF/includes.jl")
 
---
+++ src/pprint.jl
@@ -0,0 +1,70 @@
+
+using PrettyPrint
+
+export show_grammar
+
+"""
+    show_grammar(grammar_name::Symbol)
+Show a pretty description of all of the derivations in the grammar.
+"""
+function show_grammar(grammar_name::Symbol)
+    g = AllGrammars[grammar_name]
+    k = sort(collect(keys(g.derivations)))
+    for dk in k
+        d = g.derivations[dk]
+        println(d.name)
+        pprint(d.lhs)
+        println("\n")
+    end
+end
+
+function PrettyPrint.pp_impl(io, o::Empty, indent::Int)
+    r ="Empty()"
+    print(io, r)
+    indent + length(r)
+end
+
+function PrettyPrint.pp_impl(io, o::CharacterLiteral, indent::Int)
+    r = "CharacterLiteral($(repr(o.character)))"
+    print(io, r)
+    indent + length(r)
+end
+
+function PrettyPrint.pp_impl(io, o::StringLiteral, indent::Int)
+    r = "StringLiteral($(repr(o.str)))"
+    print(io, r)
+    indent + length(r)
+end
+
+function PrettyPrint.pp_impl(io, o::BNFRef, indent::Int)
+    r = "BNFRef($(repr(o.grammar_name)), $(repr(o.name)))"
+    print(io, r)
+    indent + length(r)
+end
+
+PrettyPrint.pp_impl(io, o::Sequence, indent::Int) =
+    PrettyPrint.pprint_for_seq(io, "Sequence(", ")",
+                               o.elements, indent)
+
+PrettyPrint.pp_impl(io, o::Alternatives, indent::Int) =
+    PrettyPrint.pprint_for_seq(io, "Alternatives(", ")",
+                               o.alternatives, indent)    
+
+function PrettyPrint.pp_impl(io, o::StringCollector, indent::Int)
+    r = "StringCollector("
+    print(io, r)
+    PrettyPrint.pp_impl(io, o.node, indent + 4)
+    print(io, ")")
+    indent + 2
+end
+
+function PrettyPrint.pp_impl(io, o::Constructor, indent::Int)
+    r = "Constructor("
+    print(o, r)
+    PrettyPrint.pp_impl(io, o.node, indent + 4)
+    print(io, ",\n")
+    PrettyPrint.pp_impl(io, o.constructor, indent + 4)
+    print(io, ")")
+    indent + 2
+end
+
