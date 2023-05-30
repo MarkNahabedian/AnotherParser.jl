d4626fbee6d95cc50f705032de6391ace1cd7224
MarkNahabedian <naha@mit.edu>
2023-05-30 16:44:16 -0400

    Remove empty auto-created testset.

--- test/runtests.jl
+++ test/runtests.jl
@@ -197,8 +197,3 @@ include("SemVerBNF.jl")
 include("test_note_BNFNode_location.jl")
 
 # include("../examples/BNF/test_bnf_grammar.jl")
-
-@testset "AnotherParser.jl" begin
-    # Write your tests here.
-end
-
