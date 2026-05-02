bfcd5f7f3dcaae29937faa5e94d476536a46a62b
MarkNahabedian <naha@mit.edu>
2026-05-02 09:28:27 -0400

    Add call_counter to Parser for inclusion into log messages so those messages can be matched.
    Parser now has recognize1_cache so that we don't need to reevaluate a given combination of node, input amd index.

--- src/parser.jl
+++ src/parser.jl
@@ -1,28 +1,44 @@
 export Parser, DEBUG_BNFNODES
 
+"""
+    DEBUG_BNFNODES
+
+DEBUG_BNFNODES is a list of the node uid's of nodes that should be
+`@info` logged when they match the current input at the specified
+index.
+"""
 DEBUG_BNFNODES = []
 
-struct Parser
-    recognize1_history::Dict
+mutable struct Parser
+    call_counter::Int
+    recognize1_cache::Dict
 
-    Parser() = new(Dict())
+    Parser() = new(1, Dict())
 end
 
 function recognize1(p::Parser, n, input, index, finish, context)
     dbg = n.uid in DEBUG_BNFNODES
+    p.call_counter += 1
+    call_counter = p.call_counter
+    node = pretty(n)
+    if dbg
+        @info "recognize1" call_counter node index "trying"
+    end
     key = (n.uid, input, index)
-    if haskey(p.recognize1_history, key)
+    # We should be caching the result
+    if haskey(p.recognize1_cache, key)
         if dbg
-            @info "recognize1 has already seen" n.uid index
+            @info "recognize1" call_counter cacheed_result = p.recognize1_cache[key]
         end
-        return false, nothing, index
+        return p.recognize1_cache[key]
     end
     matched, v, i = recognize(p, n, input, index, finish, context)
     if matched == true && i == index
-        p.recognize1_history[key] = true
+        p.recognize1_cache[key] = true
     end
     if dbg
-        @info "recognize1" n.uid index "returning" matched v i
+        @info "recognize1" call_counter node index "returning" matched v i
     end
+    p.recognize1_cache[key] = (matched, v, i)
     return matched, v, i
 end
