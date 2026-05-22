8461c750406f8d68d5364a523e0237db19d5ce12
MarkNahabedian <naha@mit.edu>
2026-05-22 07:52:11 -0400

    a comment about modifier keys for click events.

--- src/debug_parser.jl
+++ src/debug_parser.jl
@@ -53,6 +53,8 @@ function toggle_visibility(element) {
 
 function log_index_click_handler(event) {
     if (event instanceof PointerEvent) {
+        // When we want to add other mouse actions:
+        // event.shiftKey, event.ctrlKey, event.altKey, event.metaKey
         if (event.target.classList.contains("log-index")) {
             event.target.closest(".level").querySelectorAll(":scope > .level").forEach((element, index) => {
                 toggle_visibility(element);
