a0c4c06e2d423c6b3037a2a881ec091334c0531d
MarkNahabedian <naha@mit.edu>
2026-06-14 17:26:25 -0400

    checkpoint before topological reordering.

--- examples/XML/cst.jl
+++ examples/XML/cst.jl
@@ -9,11 +9,149 @@ export CSTNode
 
 abstract type CSTNode end
 
+const CSTMarkupDecl = Union{CSTElementDecl,
+                            CSTAttlistDecl,
+                            CSTEntityDecl,
+                            CSTNotationDecl,
+                            CSTProcessingInstruction,
+                            CSTComment}
+
+const CSTIntSubset = Union{CSTMarkupDecl, DeclSep}
+
+const CSTEntityDecl =  Union{CSTGEDecl,
+                             CSTPEDecl}
+
+const CSTReference = Union{CSTEntityRef, CSTCharRef}
+
+const CSTMisc = Union{CSTComment,
+                      CSTProcessingInstrcution,
+                      CSTWhitespace}
+
+const CSTExternalId = Union{CSTExtIdSystem,
+                            CSTExtIdPublic}
+
+const CSTPEDef = Union{CSTEntityValue,
+                       CSTExternalId}
+
+const CSTEntityDef = Union{CSTEntityValue,
+                           CSTExternalId,
+                           CSTNDataDecl)
+
+
 function test_round_trip(rulename, input)
     matches, value, i = recognize(AllGrammars[:XML][rulename], input)
     got = string(value)
     @assert input == got "$input == $got"
 end
+
+#=
+
+CSTAttValue      no test
+# CSTAttlistDecl
+# CSTAttribute
+ CSTCharData     no test
+# CSTCharRef
+# CSTComment
+# CSTDeclAttr
+ CSTDocTypeDecl   no test
+# CSTElement
+# CSTEntityRef
+# CSTElementDecl
+ CSTEq            no test
+CSTEntityValue
+# CSTExtIdPublic
+# CSTExtIdSystem
+# CSTName
+# CSTNotationDecl
+# CSTPEReference
+# CSTProcessingInstruction
+# CSTPublicID
+# CSTWhitespace
+# CSTXMLDecl
+
+* indicates that there's an assertion test below.
+
+"document"
+"Char"
+"S"                * CSTWhitespace
+"NameStartChar"
+"NameChar"
+"Name"             * CSTName
+"Names"
+"Nmtoken"
+"Nmtokens"
+"EntityValue"        CSTEntityValue
+"AttValue"           CSTAttValue
+"SystemLiteral"      AbstractString
+"PubidLiteral"       AbstractString
+PubidChar            AbstractString
+"CharData"         * AbstractString
+"Comment"          * CSTComment
+"PI"               * CSTProcessingInstruction
+"PITarget"           CSTName
+"CDSect"             CSTCharData
+"CDStart"
+"CData"              AbstractString
+"CDEnd"
+"prolog"
+"XMLDecl"          * CSTXMLDecl
+"VersionInfo"      * CSTDeclAttr
+"Eq"                 CSTEq
+"VersionNum"         AbstractString
+"Misc"
+"doctypedecl"
+"DeclSep"
+"intSubset"
+"markupdecl"
+"extSubset"
+"extSubsetDecl"
+"SDDecl"           * CSTDeclAttr
+"element"          * CSTElement
+"<AttributeList>"    Vector{Attribute}
+"STag"
+"Attribute'        * CSTAttribute
+"ETag"
+"content"
+"EmptyElemTag"
+"elementdecl"      * CSTElementDecl
+"contentspec"        AbstractString
+"children"
+"cp"
+"choice"
+"seq"
+"Mixed"
+"AttlistDecl"    * CSTAttlistDecl
+"AttDef"
+"AttType"          AbstractString
+StringType         AbstractString
+"TokenizedType"    AbstractString
+"EnumeratedType"   AbstractString
+"NotationType"
+"Enumeration"
+"DefaultDecl"
+"conditionalSect"
+"includeSect"
+"ignoreSect"
+"ignoreSectContents"
+"Ignore"
+"CharRef"          * CSTCharRef
+"Reference"
+"EntityRef"        * CSTEntityRef
+"PEReference"      * CSTPEReference
+"GEDecl"
+"PEDecl"
+"EntityDef"
+"PEDef"
+"ExternalID"       * CSTExternalId >: CSTExtIdSystem CSTExtIdPublic
+"NDataDecl"          CSTNDataDecl
+"TextDecl"
+"extParsedEnt"
+"EncodingDecl"     * CSTDeclAttr
+"EncName"            AbstractString
+"NotationDecl"     * CSTNotationDecl
+"PublicID"         * CSTPublicID
+
+=#
 
 ######################################################################
 export CSTWhitespace
@@ -270,17 +408,180 @@ struct CSTExtIdPublic <: CSTNode
     system_literal::AbstractString
 end
 
+######################################################################
+
+struct CSTNDataDecl <: CSTNode
+    whitespace1::CSTWhitespace
+    name::CSTName
+end
+
+Base.print(io::IO, n::CSTNDataDecl) =
+    print(io, n.whitespace1, n.name)
+
+######################################################################
+
 Base.print(io::IO, n::CSTExtIdPublic) =
     print(io, "PUBLIC", n.whitespace1, n.public_literal,
           n.whitespace2, n.system_literal)
 
-struct CSTDocTypeDecl  <: CSTNode
+struct CSTDocTypeDecl <: CSTNode
     whitespace1::CSTWhitespace
     name::CSTName
-    external_ids::Vector{Tuple{Union{Nothing, CSTWhitespace},
-                               Union{CSTExtIdSystem, CSTExtIdPublic}}}
+    external_ids::Vector{Tuple{CSTWhitespace, CSTExternalId}}
 
 end
+
+######################################################################
+
+struct CSTPublicID <: CSTNode
+    whitespace1::CSTWhitespace
+    literal::AbstractString
+end
+
+Base.print(io::IO, n::CSTPublicID) =
+    print(io, "PUBLIC", n.whitespace1, n.literal)
+
+struct CSTNotationDecl <: CSTNode
+    whitespace1::CSTWhitespace
+    name::CSTName
+    whitespace2::CSTWhitespace
+    id::Union{CSTExternalId, CSTPublicID}
+    whitespace3::CSTWhitespace
+end
+
+Base.print(io::IO, n::CSTNotationDecl) =
+    print(io, "<!NOTATION", n.whitespace1, n.name,
+          n.whitespace2, n.id, '>')
+
+######################################################################
+
+struct CSTDocTypeDecl <: CSTNode
+    whitespace1::CSTWhitespace
+    name::CSTName
+    whitespace2::CSTWhitespace
+    external_id::Union{CSTExtIdSystem, CSTExtIdPublic}
+    whitespace3::CSTWhitespace
+    internal_subset
+end
+
+######################################################################
+
+struct CSTElementDecl <: CSTNode
+    whitespace1::CSTWhitespace
+    name::CSTName
+    whitespace2::CSTWhitespace
+    contentspec::AbstractString
+    whitespace3::CSTWhitespace
+end
+
+Base.print(io::IO, n::CSTElementDecl) =
+    print(io, "<!ELEMENT", n.whitespace1, n.name,
+          n.whitespace2, n.contentspec,
+          n.whitespace3, '>')
+
+######################################################################
+
+struct CSTAttDef  <: CSTNode
+    whitespace1::CSTWhitespace
+    name::CSTName
+    whitespace2::CSTWhitespace
+    atttype::AbstractString
+    whitespace3::CSTWhitespace
+    DefaultDecl::AbstractString
+end
+
+Base.print(io::IO, n::CSTAttDef) =
+    print(io, n.whitespace1, n.name, n.whitespace2, n.atttype,
+          n.whitespace3, n.DefaultDecl)
+
+struct CSTAttlistDecl <: CSTNode
+    whitespace1::CSTWhitespace
+    name::CSTName
+    attdefs::Vector{CSTAttDef}
+    whitespace2::CSTWhitespace
+end
+
+Base.print(io::IO, n::CSTAttlistDecl) =
+    print(io, "<!ATTLIST", n.whitespace1, n.name, n.attdefs...,
+          n.whitespace2, ">")
+
+######################################################################
+
+struct CSTEntityValue <: CSTNode
+    quotechar::Char
+    elements::Vector{Union{AbstractString, CSTPEReference, CSTReference}}
+end
+
+Base.print(io::IO, n::CSTEntityValue) =
+    print(io, n.quotechar, n.elements..., n.quotechar)
+
+struct CSTPEDecl <: CSTNode
+    whitespace1::CSTWhitespace
+    whitespace2::CSTWhitespace
+    name::CSTName
+    whitespace3::CSTWhitespace
+    pedef::CSTPEDef
+    whitespace4::CSTWhitespace
+end
+
+Base.print(io::IO, n::CSTPEDecl) =
+    print(io, "<!ENTITY", n.whitespace1, "%", n.whitespace2, n.name,
+          n.whitespace3,
+          n.pedef,
+          n.whitespace4, ">")
+
+######################################################################
+
+struct CSTDocTypeDecl <: CSTNode
+    whitespace1::CSTWhitespace
+    name::CSTName
+    external_id::Vector{Tuple{CSTWhitespace, CSTExternalId}}
+    whitespace2::CSTWhitespace
+    internal_subset::Vector{Tuple{ CSTIntSubset,
+                                   CSTWhitespace}}
+end
+
+function Base print(io::IO, n::CSTDocTypeDecl)
+    print(io, "<!DOCTYPE", n.whitespace1, n.name)
+    for x in n.external_id
+        print(io, x[1], x[2])
+    end
+    print(io, whitespace2)
+    for x in n.internal_subset
+        print(io, "[", x[1], "]", x[2])
+    end
+    print(io, ">")
+end
+
+######################################################################
+
+struct CSTProlog <: CSTNode
+    xmldecl::Vector{CSTXMLDecl}
+    misc::Vectorr{CSTMisc}
+    dtd::Vector{Tuple{CSTDocTypeDecl
+                      CSTMisc}}
+end
+
+function Base.print(io::IO, n::CSTProlog)
+    print(io, n.xmldecl..., n.misc...)
+    for x in n.dtd
+        print(io, x[1], x[2])
+    end
+end
+
+######################################################################
+
+struct CSTDocument <: CSTNode
+    prolog::CSTProlog
+    root::CSTElement
+    misc::Vector{CSTMisc}
+end
+
+Base.print(io::IO, n::CSTDocument) =
+    print(io, n.prolog, n.root, n.misc...)
+
+
+######################################################################
 
 
 ######################################################################
@@ -291,8 +592,8 @@ end
 
 #=
 using AnotherParser
-include("examples/XML/xml.jl")
 include("examples/XML/cst.jl")
+include("examples/XML/xml.jl")
 
 # CSTWhitespace
 test_round_trip("S", " ")
@@ -332,6 +633,7 @@ test_round_trip("VersionInfo", "  version='1.0'")
 test_round_trip("EncodingDecl", "\tencoding='utf-8'")
 test_round_trip("SDDecl", "  standalone=\"yes\"")
 
+# CSTXMLDecl
 test_round_trip("XMLDecl", "<?xml version=\"1.0\"?>")
 test_round_trip("XMLDecl", "<?xml version='1.0'?>")
 test_round_trip("XMLDecl", "<?xml version = \"1.0\"?>")
@@ -340,11 +642,39 @@ test_round_trip("XMLDecl", "<?xml version='1.0' standalone='yes'?>")
 test_round_trip("XMLDecl", "<?xml version='1.0' encoding=\"UTF-8\" standalone='yes'?>")
 test_round_trip("XMLDecl", "<?xml version=\"1.0\" encoding=\"utf-8\"?>")
 
+# CSTElement
 test_round_trip("element", """<doc>
 <e a3="v3"/>
 <e a1="w1"/>
 <e a2="w2" a3="v3"/>
 </doc>""")
+
+# CSTNotationDecl
+test_round_trip("NotationDecl", "<!NOTATION n PUBLIC \"whatever\">")
+test_round_trip("NotationDecl", "<!NOTATION n1 SYSTEM \"http://www.w3.org/\">")
+
+# CSTPublicID
+test_round_trip("PublicID", "PUBLIC 'ABC123'")
+
+# CSTExternalId
+test_round_trip("ExternalID", "SYSTEM 'foo'")
+test_round_trip("ExternalID", "PUBLIC 'abc' 'foo'")
+
+# CSTElementDecl
+test_round_trip("elementdecl", "<!ELEMENT doc (#PCDATA)>")
+test_round_trip("elementdecl", "<!ELEMENT doc (foo)>")
+test_round_trip("elementdecl", "<!ELEMENT foo EMPTY>")
+
+# CSTAttlistDecl
+test_round_trip("AttlistDecl", "<!ATTLIST doc a1 CDATA \"v1\">")
+test_round_trip("AttlistDecl", "<!ATTLIST doc a1 CDATA #IMPLIED a2 CDATA #IMPLIED>")
+test_round_trip("AttlistDecl", "<!ATTLIST e a1 CDATA #IMPLIED a2 CDATA #IMPLIED a3 CDATA #IMPLIED>")
+
+# CSTPEDecl
+test_round_trip("PEDecl", "<!ENTITY % e \"<!ELEMENT doc (#PCDATA)>\">")
+test_round_trip("PEDecl", "<!ENTITY % e SYSTEM \"e.dtd\">")
+test_round_trip("PEDecl", "<!ENTITY % e PUBLIC 'whatever' \"e.dtd\">")
+test_round_trip("PEDecl", "<!ENTITY % e \"<foo>\">")
 
 =#
 
