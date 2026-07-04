76766dbcff7987b5be1bf323e515a27cb19665a3
MarkNahabedian <naha@mit.edu>
2026-07-04 09:57:38 -0400

    Integrate the W3C conformance tests into normal Julia Test facility>

--- examples/XMLExample/test/runtests.jl
+++ examples/XMLExample/test/runtests.jl
@@ -1,11 +1,21 @@
 
 using AnotherParser
 using XMLExample
+using Test
+using Glob: glob
 
 include("cst_unit_tests.jl")
 
 include("zip_file_doenload.jl")
-include("cst_conformance_testing.jl")
 ensure_w3c_test_files()
-run_conformance_tests()
+
+@testset "test $(relpath(xml, XML_CONFORMANCE_TEST_ROOT))" for xml in glob(
+    joinpath(XML_CONFORMANCE_TEST_ROOT,
+             "xmlconf/xmltest/valid/sa/*.xml"))
+    xmltext = read_decoded(xml)
+    matched, v, i = recognize(AllGrammars[:XML]["document"], xmltext)
+    @test matched
+    serialized = string(v)
+    @test xmltext == serialized
+end
 
