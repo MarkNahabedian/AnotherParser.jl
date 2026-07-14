cebcc62c913134881b97e07eb377d23f4ff74a2c
MarkNahabedian <naha@mit.edu>
2026-07-14 18:40:09 -0400

    Make XML conformance unit tests more informative.  Tests pass.  Why does this fail if we use debug_parsing instead of recognize?

--- examples/XMLExample/test/runtests.jl
+++ examples/XMLExample/test/runtests.jl
@@ -9,13 +9,39 @@ include("cst_unit_tests.jl")
 include("zip_file_doenload.jl")
 ensure_w3c_test_files()
 
+index_grammars()
+
 @testset "test $(relpath(xml, XML_CONFORMANCE_TEST_ROOT))" for xml in glob(
     joinpath(XML_CONFORMANCE_TEST_ROOT,
              "xmlconf/xmltest/valid/sa/*.xml"))
+    testnum = splitext(basename(xml))[1]
+    debug_file = joinpath(@__DIR__, "debug", testnum * ".html")
+    mkpath(joinpath(@__DIR__, "debug"))
     xmltext = read_decoded(xml)
-    matched, v, i = recognize(AllGrammars[:XML]["document"], xmltext)
+    p = Parser()
+    matched, v, i = recognize(AllGrammars[:XML]["document"], xmltext; parser = p)
+    #=
+    # Why does switching to debug_parsing cause unit test failures?
+    matched, v, i = debug_parsing(AllGrammars[:XML], "document", xmltext;
+                                  parser = p,
+                                  report_file = debug_file,
+                                  enable_debug_logging_for = x -> true)
+    =#
+    if matched == false
+        @info("Parse failures",
+              testnum,
+              failures = [ string(pf)
+                           for pf in p.parse_failures ])
+    end
     @test matched == true
     serialized = string(v)
+    if xmltext != serialized
+        @info("XML parse",
+              testnum,
+              input = xmltext,
+              output = v,
+              serialized)
+    end
     @test xmltext == serialized
 end
 
